4da234cfc89a8aa622c92908997c4ddb175dc255
rakiahaggui <rakiahaggui592@gmail.com>
2023-03-26 00:20:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,24 @@ import React from 'react';
 import './App.css';
 import Profile from './profile/Profile';
 import image from './profile/image/image1.jpeg';
+
 function App() {
   const handleAlert=(name)=>{
     alert(`profile user name is ${name} `)
-
-
+  }
+  
   return (
     <div className="App">
       <Profile
-     FullName='rakia'
-     bio='network administrator'
-     profession=' Administrator'
-     handleAlert={handleAlert}
-     >
-    {image}
-
-     </Profile>
-
+        FullName='rakia'
+        bio='network administrator'
+        profession=' Administrator'
+        handleAlert={handleAlert}
+      >
+        {image}
+      </Profile>
     </div>
   );
 }
-  }
+
 export default App;
